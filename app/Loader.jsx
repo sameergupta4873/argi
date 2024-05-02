4bc63230a941093b8cdbf0624eaa581159339e72
tejash <{ID}+{username}@users.noreply.github.com>
2024-05-02 12:05:54 +0530

    New Logos

--- app/Loader.jsx
+++ app/Loader.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import React, { use, useEffect } from 'react'
+import { Icons } from './Logos';
 
 const Loader = () => {
     const [height, setHeight] = React.useState('120vh');
@@ -17,8 +18,10 @@ const Loader = () => {
   return (
     <>
     {show ? <div className={`fixed top-0 h-[110vh] w-[100vw] bg-[#FFF] z-[500] transition-all duration-300 ease-in-out flex flex-col justify-center items-center pt-[4rem] max-md:pt-[0rem]`}>
-        <Image src="/Loader.webp" alt="Loader" className='max-md:scale-75' height={275} width={275} priority />
-        <div className='w-full flex justify-center items-center mt-12 max-md:mt-0'>
+ 
+        <div className="hidden sm:block"><Icons.SplashScreenWeb /></div>
+        <div className="sm:hidden mb-5"><Icons.SplashScreenMobile /></div>
+        <div className='w-full flex justify-center items-center mt-2 max-md:mt-0'>
             <h1 className="text-[#007947] text-[2.5rem] font-normal space-x-[0.0625rem] taviraj max-md:text-[1.25rem]">Better Environment</h1>
             <h1 className="text-[#000] text-[2.5rem] font-normal space-x-[0.0625rem] taviraj max-md:text-[1.25rem]">, Better Tomorrow</h1>
         </div>
